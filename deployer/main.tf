de3a37f0a7a19630fd9090ab00b3c03adafcdf94
Tanay Shah <makerofuniverse1134@gmail.com>
2024-04-20 20:24:33 -0400

    terraform updates

--- deployer/main.tf
+++ deployer/main.tf
@@ -1,10 +1,10 @@
 resource "local_file" "dog" {
-  content  = "This is a pet file"
+  content  = "This is a dog file"
   filename = "${path.module}/dog.txt"
   
 }
 resource "local_file" "cat" {
-  content  = "This is a pet file"
+  content  = "This is a cat file"
   filename = "${path.module}/cat.txt"
   
 }
